dac7595fc504961ea5e020b0ebf61b8ba0102963
Bartek Krupa <krupa1.bk@gmail.com>
2020-03-26 00:01:53 +0100

    Updates

---
+++ precache-manifest.e1be79d2ffc8ae95ceb4b67ae295b6fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b40831e43fe6c2ac129d2deafe61cd",
+    "url": "/mdolatowska/index.html"
+  },
+  {
+    "revision": "0e711647840bbfcd5bb4",
+    "url": "/mdolatowska/static/css/main.63ce8b93.chunk.css"
+  },
+  {
+    "revision": "6e3b61d20eaf723c3387",
+    "url": "/mdolatowska/static/js/2.eedfe8df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mdolatowska/static/js/2.eedfe8df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e711647840bbfcd5bb4",
+    "url": "/mdolatowska/static/js/main.6b5e52ba.chunk.js"
+  },
+  {
+    "revision": "5ebd716aedcd4d15d335",
+    "url": "/mdolatowska/static/js/runtime-main.9a036c38.js"
+  }
+]);
